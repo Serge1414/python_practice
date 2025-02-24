7b1062449862e15378ef4653de4697b72a4ba5db
Serge1414 <pidtesany@gmail.com>
2025-02-24 20:07:42 +0200

    read password in main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 def func():
     print('hello')
+
+
+filename = 'password.txt'
+
+with open(filename, 'r') as file:
+    lines = file.readlines()
+
+
+line1 = lines[0]
+name, password = line1.split()
+
+print(password)
