3b0bfb1cea8932ef9b8f6bf019783338e50f0eec
Serge1414 <pidtesany@gmail.com>
2025-03-04 17:21:33 +0200

    Створення class PayPalPayment

--- hm_04.03.py
+++ hm_04.03.py
@@ -4,3 +4,11 @@ class CreditCardPayment:
 
     def pay(self, amount):
         print(f"Оплата карткою {amount}{self.currency}")
+
+
+class PayPalPayment:
+    def __init__(self, currency):
+        self.currency = currency
+
+    def pay(self, amount):
+        print(f"Оплата PayPal {amount}{self.currency}")
