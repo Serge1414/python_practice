e16d568da756b35de04ac5b57f7bff743f842963
Serge1414 <pidtesany@gmail.com>
2025-03-06 16:40:01 +0200

    Створили методи

--- HW_06_03.py
+++ HW_06_03.py
@@ -40,3 +40,13 @@ class Recipe:
         self.ingredients = ingredients
         self.text = text
         self.time = time
+
+    def __str__(self):
+        return self.name
+
+    def __contains__(self, item):
+        return item in self.ingredients
+
+    def __gt__(self, other):
+        return self.time > other.time
+
